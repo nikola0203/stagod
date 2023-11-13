f1a935ed819a414c4d3d9ea744608485b8d58cfa
koki1993 <kokihisar@gmail.com>
2023-11-13 22:52:14 +0100

    footer ckeanup

--- themes/advanced-wp-starter-theme/footer.php
+++ themes/advanced-wp-starter-theme/footer.php
@@ -16,19 +16,6 @@ use Awpt\Custom\Footer;
 		<div class="container">
 			<?php Footer::copy(); ?>
 		</div>
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'awpt' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'awpt' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'awpt' ), 'awpt', '<a href="https://github.com/nikola0203/">Nikola Milosevic</a>' );
-				?>
-		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
