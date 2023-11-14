ad0706ce7788f8180717d689493d5674c92793a5
koki1993 <kokihisar@gmail.com>
2023-11-14 17:32:49 +0100

    Variables, typography, buttons and links changes

--- themes/advanced-wp-starter-theme/footer.php
+++ themes/advanced-wp-starter-theme/footer.php
@@ -15,6 +15,12 @@ use Awpt\Custom\Footer;
 	<footer id="colophon" class="site-footer">
 		<div class="container">
 			<?php Footer::copy(); ?>
+			<button class="btn btn-primary">Test</button>
+			<button class="btn btn-transparent">Test</button>
+			<button class="btn btn-orange-ghost">Test</button>
+			<a href="" class="txt-primary">Test link primary</a>
+			<a href="" class="txt-secondary">Test link secondary</a>
+			<a href="" class="txt-small">Test link small</a>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
