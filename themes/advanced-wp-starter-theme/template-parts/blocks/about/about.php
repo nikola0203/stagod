5534aa31f0059f4ae5cef1d820bd10f463d61516
koki1993 <kokihisar@gmail.com>
2023-11-27 16:19:09 +0100

    markup update

--- themes/advanced-wp-starter-theme/template-parts/blocks/about/about.php
+++ themes/advanced-wp-starter-theme/template-parts/blocks/about/about.php
@@ -3,23 +3,26 @@
  * Template part for displaying page About block.
  *
  */
-
+$args = get_field('about');
+if ( ! empty( $args )):
 ?>
-<section class="about text-center py-12 py-lg-22 txt-white">
-  <div class="container">
-    <div class="row">
-      <?php
-      $args = get_field('about');
-      foreach( $args as $arg):
+  <section class="about text-center py-12 py-lg-22 txt-white">
+    <div class="container">
+      <div class="row">
+        <?php
+        foreach( $args as $arg):
+          ?>
+          <div class="col-12 col-lg-4">
+            <img src="<?php echo esc_url($arg['image']['url']); ?>" alt="" class="rounded-circle mb-10">
+            <h4 class="mb-2"><?php echo esc_html($arg['title']); ?></h4>
+            <p class="mb-8 mb-lg-0 p-small"><?php echo esc_html($arg['text']); ?></p>
+          </div>
+        <?php 
+        endforeach;
         ?>
-        <div class="col-12 col-lg-4">
-          <img src="<?php echo $arg['image']['url'] ?>" alt="" class="rounded-circle mb-10">
-          <h4 class="mb-2"><?php echo $arg['title']; ?></h4>
-          <p class="mb-8 mb-lg-0 p-small"><?php echo $arg['text']; ?></p>
-        </div>
-      <?php 
-      endforeach;
-      ?>
+      </div>
     </div>
-  </div>
-</section>
+  </section>
+<?php
+endif;
+?>
--- themes/advanced-wp-starter-theme/template-parts/blocks/info/info.php
+++ themes/advanced-wp-starter-theme/template-parts/blocks/info/info.php
@@ -5,23 +5,25 @@
  */
 
 $args = get_field('info');
-$count_args = count( $args );
-foreach( $args as $arg_key => $arg ):
-?>
-  <section class="info py-12 py-lg-22 <?php echo ( $arg_key == $count_args-1 ) ? 'info-two' : ''; ?>">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-lg-6 <?php echo ( $arg_key == $count_args-1 ) ? 'order-lg-1' : ''; ?> d-flex flex-column justify-content-center">
-          <h2 class="mb-6 fw-bold"><?php echo $arg['title']; ?></h2>
-          <p class="mb-8 p-big"><?php echo $arg['text']; ?></p>
-          <a href="<?php echo $arg['link']['url'] ?>"><input type="button" value="<?php echo $arg['link']['title']; ?>" class="btn btn-primary mb-8 mb-lg-0"></a>
-        </div>
-        <div class="col-12 col-lg-6 <?php echo ( $arg_key == $count_args-1 ) ? 'text-lg-start' : 'text-lg-end'; ?>">
-          <img src="<?php echo $arg['image']['url']; ?>" alt="" class="w-lg-75">
+if ( ! empty( $args )):
+  $count_args = count( $args );
+  foreach( $args as $arg_key => $arg ):
+  ?>
+    <section class="info py-12 py-lg-22 <?php echo ( $arg_key == $count_args-1 ) ? 'info-two' : ''; ?>">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-lg-6 d-flex flex-column justify-content-center <?php echo ( $arg_key == $count_args-1 ) ? 'order-lg-1' : ''; ?> ">
+            <h2 class="mb-6"><?php echo esc_html($arg['title']); ?></h2>
+            <p class="mb-8 p-small"><?php echo esc_html($arg['text']); ?></p>
+            <a href="<?php echo $arg['link']['url'] ?>"><input type="button" value="<?php echo esc_html($arg['link']['title']); ?>" class="btn btn-primary mb-8 mb-lg-0"></a>
+          </div>
+          <div class="col-12 col-lg-6 <?php echo ( $arg_key == $count_args-1 ) ? 'text-lg-start' : 'text-lg-end'; ?>">
+            <img src="<?php echo esc_url($arg['image']['url']); ?>" alt="" class="w-lg-75">
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-  <?php
-endforeach;
+    </section>
+    <?php
+  endforeach;
+endif;
 ?>  
