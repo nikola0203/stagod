396646d515784c60caab0e740c538ad88a2607c0
koki1993 <kokihisar@gmail.com>
2024-03-25 10:08:51 +0100

    minor char style

--- themes/advanced-wp-starter-theme/template-parts/author/private/chat.php
+++ themes/advanced-wp-starter-theme/template-parts/author/private/chat.php
@@ -13,11 +13,11 @@ $messages          = $chat->get_message_content( $chat_id );
 
 if ( $user->user_firstname && $user->user_lastname ) :
   ?>
-  <h2 class="h3 mb-6"><a href="<?php echo esc_url( get_author_posts_url( get_current_user_id() ) . 'messages' ); ?>"><?php echo icon_arrow_down(); ?></a> <?php esc_html_e( $user->user_firstname . '&nbsp;' . $user->user_lastname ); ?></h2>
+  <h2 class="chat-title h3 mb-6"><a href="<?php echo esc_url( get_author_posts_url( get_current_user_id() ) . 'messages' ); ?>"><?php echo icon_arrow_down(); ?></a> <?php esc_html_e( $user->user_firstname . '&nbsp;' . $user->user_lastname ); ?></h2>
   <?php
 endif;
 ?>
-<div class="chat border rounded-4 p-4 p-xl-8 bg-info mb-2">
+<div class="chat rounded-4 p-4 p-xl-8 bg-info mb-2">
   <?php
   if ( ! empty( $messages ) ) :
     foreach ( $messages as $message ) :
@@ -26,6 +26,7 @@ endif;
         <div class="col-lg-7">
           <div class="bg-white rounded-4 mb-8 p-6">
             <?php echo wp_kses_post( $message['content'] ); ?>
+            <?php echo wp_kses_post( $message['date'] ); ?>
           </div>
         </div>
       </div>
--- themes/advanced-wp-starter-theme/template-parts/author/private/info.php
+++ themes/advanced-wp-starter-theme/template-parts/author/private/info.php
@@ -16,13 +16,17 @@ $saved_url         = get_author_posts_url( $page_id ) . 'saved';
 $edit_account_url  = get_author_posts_url( $page_id ) . 'edit-account';
 $edit_profile_url  = get_author_posts_url( $page_id ) . 'edit-profile';
 ?>
-<div class="author-info-wrapper border rounded-4">
+<div class="author-info-wrapper border border-light rounded-4 bg-white">
   <div class="author-info-top d-flex flex-column justify-content-center align-items-center p-lg-8">
+    <div class="img-wrapper-profile mb-2">
+      <?php
+      Acf::image( $image, 'large', 'rounded-circle object-fit-cover' );
+      ?>
+    </div>
     <?php
-    Acf::image( $image );
     if ( $current_user->user_firstname && $current_user->user_lastname ) :
       ?>
-      <h2 class="h4 mb-2"><?php esc_html_e( $current_user->user_firstname . '&nbsp;' . $current_user->user_lastname ); ?></h2>
+      <h2 class="h4 mb-6"><?php esc_html_e( $current_user->user_firstname . '&nbsp;' . $current_user->user_lastname ); ?></h2>
       <?php
     endif;
     ?>
@@ -30,12 +34,12 @@ $edit_profile_url  = get_author_posts_url( $page_id ) . 'edit-profile';
   </div>
   <div class="author-info-bottom">
     <ul>
-      <li class="mb-4 mb-xl-8 <?php echo ( $current_url == $messages_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $messages_url ); ?>">Poruke <?php echo icon_arrow_down(); ?></a></li>
-      <li class="mb-4 mb-xl-8 <?php echo ( $current_url == $notifications_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $notifications_url ); ?>">Obavestenja <?php echo icon_arrow_down(); ?></a></li>
-      <li class="mb-4 mb-xl-8 <?php echo ( $current_url == $ratings_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $ratings_url ); ?>">Ocene <?php echo icon_arrow_down(); ?></a></li>
-      <li class="mb-4 mb-xl-8 <?php echo ( $current_url == $saved_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $saved_url ); ?>">Sacuvano <?php echo icon_arrow_down(); ?></a></li>
-      <li class="mb-4 mb-xl-8 <?php echo ( $current_url == $edit_account_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $edit_account_url ); ?>">Podesavanje naloga <?php echo icon_arrow_down(); ?></a></li>
-      <li class="mb-4 mb-xl-8 <?php echo ( $current_url == $edit_profile_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $edit_profile_url ); ?>">Podesavanje profila usluga <?php echo icon_arrow_down(); ?></a></li>
+      <li class="mb-6 mb-xl-8 <?php echo ( $current_url == $messages_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $messages_url ); ?>">Poruke <?php echo icon_arrow_down(); ?></a></li>
+      <li class="mb-6 mb-xl-8 <?php echo ( $current_url == $notifications_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $notifications_url ); ?>">Obavestenja <?php echo icon_arrow_down(); ?></a></li>
+      <li class="mb-6 mb-xl-8 <?php echo ( $current_url == $ratings_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $ratings_url ); ?>">Ocene <?php echo icon_arrow_down(); ?></a></li>
+      <li class="mb-6 mb-xl-8 <?php echo ( $current_url == $saved_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $saved_url ); ?>">Sacuvano <?php echo icon_arrow_down(); ?></a></li>
+      <li class="mb-6 mb-xl-8 <?php echo ( $current_url == $edit_account_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $edit_account_url ); ?>">Podesavanje naloga <?php echo icon_arrow_down(); ?></a></li>
+      <li class="mb-6 mb-xl-8 <?php echo ( $current_url == $edit_profile_url ) ? 'active' : ''; ?>"><a href="<?php echo esc_url( $edit_profile_url ); ?>">Podesavanje profila usluga <?php echo icon_arrow_down(); ?></a></li>
     </ul>
   </div>
 </div>
